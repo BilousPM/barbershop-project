4d9566f2b55bfcca9f94cfdd5277119c4f43a802
Petro Bilous <111942988+BilousPM@users.noreply.github.com>
2024-02-29 10:07:43 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# barbershop
+
+https://www.figma.com/file/KvsCxwFjYtoPsotMJWJ1ul/Barbershop-EN-(Copy)?type=design&node-id=1374-32&mode=design&t=1VsI6PyY8buglprU-0
---
+++ js/header-scroll.js
@@ -0,0 +1,13 @@
+window.onscroll = () => changeHeaderBacgraund();
+
+function changeHeaderBacgraund() {
+  const header = document.querySelector("[data-header]");
+  const headerOffsetTrigger = header.offsetHeight;
+  const pageOffset = window.pageYOffset;
+
+  if (pageOffset > headerOffsetTrigger) {
+    header.classList.add("no-transparency");
+  } else {
+    header.classList.remove("no-transparency");
+  }
+}
